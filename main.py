e1f36886c0afd4c0550f52393c61ef829651090b
Angel <angrodri@gmail.com>
2023-04-07 23:29:12 -0400

    c

---
+++ main.py
@@ -0,0 +1,26 @@
+print("dominican park")
+print("dominican 1")
+
+# soy un comentario 
+print("dominican 2")
+
+"""
+texto qno se va a interpretar
+"""
+
+# variables 
+texto = "cosas de la vida"
+nombre = "Angel Rodriguez"
+altura = "170cm"
+year = 2002
+ 
+print (texto)
+print (nombre)
+
+print(f"{texto} - {nombre} - {altura} - {str(year)}")
+
+# entrada 
+sitioweb = input("cual es tu sitio web?: ")
+print(sitioweb)
+
+
